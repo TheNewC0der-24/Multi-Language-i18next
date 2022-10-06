576da04ee332d4a3b801cd100220e3c24656364b
Bhavya Khurana <69009796+TheNewC0der-24@users.noreply.github.com>
2022-10-06 18:23:24 +0530

    Configure i18next, browser language detector, and http backend

--- src/index.js
+++ src/index.js
@@ -1,7 +1,36 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import i18n from "i18next";
+import { initReactI18next } from "react-i18next";
+import LanguageDetector from 'i18next-browser-languagedetector';
+import HttpApi from 'i18next-http-backend';
+
+import 'bootstrap/dist/js/bootstrap.js';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 import './index.css';
 import App from './App';
+
+i18n
+  .use(initReactI18next) // passes i18n down to react-i18next
+  .use(LanguageDetector)
+  .use(HttpApi)
+  .init({
+    supportedLngs: ['en', 'ar', 'fr'],
+    fallbackLng: "en",
+    debug: false,
+    // Options for language detector
+    detection: {
+      order: ['path', 'cookie', 'htmlTag', 'localStorage', , 'subdomain'],
+      caches: ['cookie']
+    },
+    backend: {
+      loadPath: '/assets/locales/{{lng}}/translation.json'
+    },
+    react: {
+      useSuspense: false
+    }
+  });
 
 ReactDOM.render(
   <React.StrictMode>
